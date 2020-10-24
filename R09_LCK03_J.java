7b53443adb3d76188cdc7a9825f847b0a777ed17
amiranda55 <58144219+amiranda55@users.noreply.github.com>
2020-10-24 16:49:20 -0400

    Update R09_LCK03_J.java

--- R09_LCK03_J.java
+++ R09_LCK03_J.java
@@ -1,3 +1,5 @@
+// Rule 09. Locking
+// LCK03_J. Do not synchronize on the intrinsic locks of high-level concurrency objects
 private final Lock lock = new ReentrantLock();
  
 public void doSomething() {
