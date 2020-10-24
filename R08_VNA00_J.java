b3035aefcb26fb73fac423e8e0f8b27ef01f6581
amiranda55 <58144219+amiranda55@users.noreply.github.com>
2020-10-24 16:48:41 -0400

    Update R08_VNA00_J.java

--- R08_VNA00_J.java
+++ R08_VNA00_J.java
@@ -1,3 +1,5 @@
+// Rule 08. Visibility and Atomicity
+// VNA00_J. Ensure visibility when accessing shared primitive variables
 final class ControlledStop implements Runnable {
   private volatile boolean done = false;
   
